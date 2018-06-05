ca661107e1758d887c7372b3afd31624e83c08c8
Don Denton <don@happycollision.com>
2018-06-05 15:44:52 -0500

    Change phrases

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -14,7 +14,8 @@ export class ApiService {
     consoleOnce.warn('Using fake data for `getPhrases`');
 
     const phrases: Phrase[] = [
-      { id: '1', text: 'Hello' },
+      { id: '1', text: 'Hello from [YOUR CITY], [YOUR STATE]! You are watching Me TV, Channel 23!' },
+      { id: '2', text: 'That\'s why I choose Me TV news.' },
     ];
     return from([phrases]).pipe(delay(1000)).toPromise();
   }
