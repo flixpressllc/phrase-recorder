9a77641c61e17d60ba273e0c5bd4501e10ba457a
Don Denton <don@happycollision.com>
2018-06-05 14:16:07 -0500

    simple phrase choosing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './services/api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  phrases: Phrase[] = [];
+  chosenPhrase: Phrase = null;
+
+  constructor(
+    private api: ApiService,
+  ) {}
+
+  ngOnInit() {
+    this.api.getPhrases().then((phrases) => this.phrases = phrases);
+  }
+
+  choosePhrase(phrase: Phrase) {
+    this.chosenPhrase = phrase;
+  }
 }
