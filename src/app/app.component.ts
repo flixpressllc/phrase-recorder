0dabc1c529bface0299f19e4ee2505113e24289e
Don Denton <don@happycollision.com>
2018-06-05 15:10:15 -0500

    task for recording

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ApiService } from './services/api.service';
-import { createTask, TaskObject } from 'angular-concurrency';
+import { createTask, TaskObject, timeout } from 'angular-concurrency';
+import { RecordRTCComponent } from './modules/recorder/recorder.module';
+import { RecorderService } from './modules/recorder/services/recorder.service';
 
 @Component({
   selector: 'app-root',
@@ -10,14 +12,25 @@ import { createTask, TaskObject } from 'angular-concurrency';
 export class AppComponent implements OnInit {
   phrases: Phrase[] = [];
   chosenPhrase: Phrase = null;
+  @ViewChild('recorder') recorder: RecordRTCComponent;
 
   populatePhrases: TaskObject = (createTask.call(this, function* (this: AppComponent) {
     const phrases = yield this.api.getPhrases();
     this.phrases = phrases;
   }) as TaskObject).setSchedule('restart');
 
+  startTimedRecording: TaskObject = (createTask.call(this, function* (this: AppComponent) {
+    if (!this.choosePhrase) { return; }
+    const recordTime = this.getRecordTime();
+    this.recorder.startRecording();
+    yield timeout(recordTime);
+    yield this.recorder.stopRecording();
+    return this.recordService.lastSuccessfulRecording;
+  }) as TaskObject).setSchedule('drop');
+
   constructor(
     private api: ApiService,
+    private recordService: RecorderService,
   ) {}
 
   ngOnInit() {
@@ -26,5 +39,9 @@ export class AppComponent implements OnInit {
 
   choosePhrase(phrase: Phrase) {
     this.chosenPhrase = phrase;
+  }
+
+  getRecordTime(): number {
+    return 1000;
   }
 }
