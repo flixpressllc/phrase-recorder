a19252ca8112fe01f5705252cdd42415c5a4f311
Don Denton <don@happycollision.com>
2018-06-05 17:06:54 -0500

    create ios workflow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { ApiService } from './services/api.service';
 import { createTask, TaskObject, timeout } from 'angular-concurrency';
 import { RecordRTCComponent } from './modules/recorder/recorder.module';
 import { RecorderService } from './modules/recorder/services/recorder.service';
+import { DeviceDetectorService } from 'ngx-device-detector';
 
 const COUNTDOWN_SECONDS = 3;
 
@@ -15,11 +16,25 @@ export class AppComponent implements OnInit {
   phrases: Phrase[] = [];
   chosenPhrase: Phrase = null;
   countdown: number = null;
+  isIPhone = false;
+  chosenFile: File = null;
+  recordTimeInSeconds = 0;
   @ViewChild('recorder') recorder: RecordRTCComponent;
+  @ViewChild('iosFileChooser') fileChooser: ElementRef<HTMLInputElement>;
 
   populatePhrases: TaskObject = (createTask.call(this, function* (this: AppComponent) {
     const phrases = yield this.api.getPhrases();
     this.phrases = phrases;
+  }) as TaskObject).setSchedule('restart');
+
+  watchIosFileChooser: TaskObject = (createTask.call(this, function* (this: AppComponent) {
+    while (!this.fileChooser) {
+      yield timeout(1000);
+    }
+    while (this.fileChooser.nativeElement.files.length < 1) {
+      yield timeout(500);
+    }
+    return this.chosenFile = this.fileChooser.nativeElement.files[0];
   }) as TaskObject).setSchedule('restart');
 
   startTimedRecording: TaskObject = (createTask.call(this, function* (this: AppComponent) {
@@ -46,14 +61,22 @@ export class AppComponent implements OnInit {
   constructor(
     private api: ApiService,
     private recordService: RecorderService,
+    private detector: DeviceDetectorService,
   ) {}
 
   ngOnInit() {
     this.populatePhrases.perform();
+    this.discoverDevice();
+  }
+
+  discoverDevice() {
+    this.isIPhone = this.detector.getDeviceInfo().device === 'iphone';
+    if (this.isIPhone) { this.watchIosFileChooser.perform(); }
   }
 
   choosePhrase(phrase: Phrase) {
     this.chosenPhrase = phrase;
+    this.recordTimeInSeconds = this.getRecordTime() / 1000;
   }
 
   getRecordTime(): number {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { DeviceDetectorModule } from 'ngx-device-detector';
 
 import { AppComponent } from './app.component';
 import { RecorderModule } from './modules/recorder/recorder.module';
@@ -11,6 +12,7 @@ import { RecorderModule } from './modules/recorder/recorder.module';
   imports: [
     BrowserModule,
     RecorderModule,
+    DeviceDetectorModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent],
